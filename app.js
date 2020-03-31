2e99c2934d7f7872fa078dddd302ccc50e669ed1
drumky <axeljcob@gmail.com>
2020-03-31 18:38:08 +0800

    Set functionality: Input(done), Error(done)

--- app.js
+++ app.js
@@ -15,5 +15,40 @@ function calculateResults(e) {
   const totalPayment = document.getElementById("total-payment");
   const totalInterest = document.getElementById("total-interest");
 
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+  } else {
+    showError("Please check your numbers ..");
+  }
+
   e.preventDefault();
+}
+
+//Show error
+function showError(error) {
+  // Create a Div
+  const errorDiv = document.createElement("div");
+
+  // Get Element
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
+
+  // Add class
+  errorDiv.className = "alert alert-danger";
+
+  // Create Text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
 }
